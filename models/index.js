fa69bc6695f674a552ee8b4b5de4bc8fe8d4d237
MarcM <marina.ved80gmail.com>
2024-07-22 22:04:36 -0600

    add more tables to db

--- models/index.js
+++ models/index.js
@@ -1,16 +1,35 @@
-// const User = require('./User');
+const Operator = require('./Operator.js');
 const Machine = require('./Machine.js');
 const Sample = require('./Sample.js');
+const MachineAccess = require('./MachineAccess.js');
+
 
 Machine.hasMany(Samples, {
     foreignKey: 'machine_id',
     onDelete: 'CASCADE',
-  });
+});
   
-  Sample.belongsTo(Machine, {
+Sample.belongsTo(Machine, {
     foreignKey: 'machine_id',
-  });
+});
   
- 
-  module.exports = { Machine, Sample };
-// module.exports = { User };
+Operator.hasMany(MachineAccess, {
+    foreignKey: 'operator_id',
+    onDelete: 'CASCADE',
+});
+
+MachineAccess.belongsTo(Operator, {
+  foreignKey: 'operator_id',
+});
+
+Machine.hasMany(MachineAccess, {
+  foreignKey: 'machine_id',
+  onDelete: 'CASCADE',
+});
+
+MachineAccess.belongsTo(Machine, {
+  foreignKey: 'machine_id',
+});
+
+  module.exports = { Machine, Sample, Operator, MachineAccess };
+
